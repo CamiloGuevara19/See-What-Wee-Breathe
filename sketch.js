b13c5a79cb457b621afcd62d77ed8aeb4aca0349
CamiloGuevara19 <camilogr0419@gmail.com>
2022-05-24 01:42:52 -0500

    added sounds

--- sketch.js
+++ sketch.js
@@ -12,7 +12,67 @@ function setup() {
     arm.push(new waveMaker());
   }
 
+  switch (localStorage.getItem("valor")) {
+    case "130":
+      sound3.play();
+      sound3.setVolume(0.5);
+    break;
 
+    case "370":
+      sound8.play();
+      sound8.setVolume(0.5);
+    break;
+
+    case "400":
+      sound9.play();
+      sound9.setVolume(0.5);
+    break;
+
+    case "180":
+      sound4.play();
+      sound4.setVolume(0.5);
+    break;
+
+    case "230":
+      sound5.play();
+      sound5.setVolume(0.5);
+    break;
+
+    case "100":
+      sound2.play();
+      sound2.setVolume(0.5);
+    break;
+
+    case "300":
+      sound7.play();
+      sound7.setVolume(0.5);
+    break;
+
+    case "240":
+      sound6.play();
+      sound6.setVolume(0.5);
+    break;
+
+    case "50":
+      sound1.play();
+      sound1.setVolume(0.5);
+    break;
+  
+    default:
+      break;
+  }
+}
+
+function preload(){
+  sound1 = loadSound("./data/sounds/1.wav");
+  sound2 = loadSound("./data/sounds/2.wav");
+  sound3 = loadSound("./data/sounds/3.wav");
+  sound4 = loadSound("./data/sounds/4.mp3");
+  sound5 = loadSound("./data/sounds/5.wav");
+  sound6 = loadSound("./data/sounds/6.wav");
+  sound7 = loadSound("./data/sounds/7.wav");
+  sound8 = loadSound("./data/sounds/8.wav");
+  sound9 = loadSound("./data/sounds/9.flac");
 }
 
 function draw() {
@@ -61,38 +121,56 @@ function waveMaker(){
     switch (localStorage.getItem("valor")) {
       case "130":
         fill(214-colr-50,255-colr-50,183-colr-50,125);
+        //sound3.play();
+        //sound3.setVolume(0.5);
       break;
 
       case "370":
         fill(231-colr-50,33-colr-50,29-colr-50,125);
+        //sound8.play();
+        //sound8.setVolume(0.5);
       break;
 
       case "400":
         fill(232-colr-50,27-colr-50,23-colr-50,125);
+        //sound9.play();
+        //sound9.setVolume(0.5);
       break;
 
       case "180":
         fill(239-colr-50,255-colr-50,174-colr-50,125);
+        //sound4.play();
+        //sound4.setVolume(0.5);
       break;
 
       case "230":
         fill(243-colr-50,227-colr-50,68-colr-50,125);
+        //sound5.play();
+        //sound5.setVolume(0.5);
       break;
 
       case "100":
         fill(122-colr-50,231-colr-50,199-colr-50,125);
+        //sound2.play();
+        //sound2.setVolume(0.5);
       break;
 
       case "300":
         fill(232-colr-50,64-colr-50,61-colr-50,125);
+        //sound7.play();
+        //sound7.setVolume(0.5);
       break;
 
       case "240":
         fill(254-colr-50,145-colr-50,57-colr-50,125);
+        //sound6.play();
+        //sound6.setVolume(0.5);
       break;
 
       case "50":
         fill(148-colr-50,193-colr-50,241-colr-50,125);
+        //sound1.play();
+        //sound1.setVolume(0.5);
       break;
     
       default:
@@ -106,7 +184,7 @@ function waveMaker(){
 function windowResized(){
   w = windowWidth;
   h = windowHeight;
- resizeCanvas(w, h);
+  resizeCanvas(w, h);
 }
 
 
